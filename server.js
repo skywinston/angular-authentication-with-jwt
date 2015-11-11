59b2fc0e24ff38e14f883c9b4ba02eb445042f02
Sky Winston <skywinston@gmail.com>
2015-11-10 21:56:07 -0700

    I now have tokenization occuring with jwt upon a successful login

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+require('dotenv').load();
 var express = require('express');
 var faker = require('faker');
 var cors = require('cors');
+var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken')
+
+// In lieu of a database, we're just going to include a user object here.
+var user = {
+  username: 'skywinston',
+  password: 'p'
+};
 
 var app = express();
 
 // Enable Cross-Origin Resource Sharing
 app.use(cors());
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/random-user', function(req, res){
@@ -14,6 +24,35 @@ app.get('/random-user', function(req, res){
   res.json(user);
 });
 
+// Use the authenticate middleware function to see if the user exists.
+// We've hardcoded a user insteaad of making a call to a DB.
+app.post('/login', authenticate, function(req, res){
+  console.log("Authenticated!");
+  // Encode the user with JWT
+  // We are not including everything that the JWT spec recommends, so we would actually need more here.
+  var token = jwt.sign({
+    username: user.username
+  }, process.env.JWTTOKEN)
+  res.send({
+    token: token,
+    user: user // You would never actually send this in the payload as it has the password, but whatevs...
+  });
+})
+
 app.listen(3000, function(){
   console.log("App listening on port 3000...");
 });
+
+// UTIL FUNCTIONS
+
+function authenticate(req, res, next){
+  var body = req.body;
+  console.log(body);
+  if (!body.username || !body.password){
+    res.status(400).send('Must provide username or password');
+  }
+  if (body.username !== user.username || body.password !== user.password){
+    res.status(401).send('Username or password incorrect');
+  }
+  next();
+}
